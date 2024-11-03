2ede54b0ac2ecb682caaf571abd801a0be95c82f
patty 💜 <87675545+p4tt7@users.noreply.github.com>
2024-11-03 18:21:35 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter an integer:");
+int entero = int.Parse(Console.ReadLine());
+int contador = 1;
+int factorial = 1;
+
+if(entero<0){
+    Console.WriteLine("Undefined");
+}
+
+else{
+    while(contador <= entero){
+        factorial *= contador;
+        contador++;
+    }
+
+    Console.WriteLine(factorial);    
+}
+
+
